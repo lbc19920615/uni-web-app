26b1ad780d01c0d9479c794d50490ffd318598c4
LBC-PC\lbc15 <1585638808@qq.com>
2023-07-10 12:47:28 +0800

    update

--- src/pages/test/gen.vue
+++ src/pages/test/gen.vue
@@ -2,8 +2,8 @@
   <template>
 
   <page-wrapper-detail>
-    <view class="bgc-background"><scroll-view __eid__="id111"  scroll-y="true" class="height-500"><view __eid__="id1212"  class="overflow-hidden height-360"><view __eid__="id11111" >简单demo</view><view __eid__="id11112" >{{page.a}} + {{page.b}} = {{page.c}}</view></view><button __eid__="id1213" >button</button></scroll-view></view>
-  </page-wrapper-detail>
+<scroll-view __eid__="id111"  scroll-y="true" class="h-full"><video __eid__="id1223"  class="w-full" src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/2minute-demo.mp4"></video><view __eid__="id__ojst8a"  class="" style="color:rgba(242, 244, 245, 1);background-color:rgba(37, 142, 228, 1)">测试</view><view __eid__="id1212"  class="overflow-hidden height-260"><view __eid__="id11111"  class="">简单demo</view><view __eid__="id11112"  class="">{{state.a}} + {{state.b}} = {{state.c}}</view></view><button __eid__="id1213"  class="" @click="state.s">button</button><view __eid__="idlnr5jbpb07"  class="">是的是的是</view></scroll-view>
+ </page-wrapper-detail>
   
 </template>
 
@@ -13,26 +13,17 @@
       const $frame = $app.globalData.frame;
       const $getStore = $frame.getStore;
 
-import { injectStore } from "@/frame/storeMan";
 
 
 
-@injectStore('page')
-class def{
- a = 1;
- b = 2;
-get c() {
-  return this.a+this.b
-}
-s() {
-  this.a = this.a + 1
-}
+      let {ins: state, refs} = $getStore('state');
+console.log(state);
+      
 
-}
-
-let {ins: page, refs} = $getStore('page');
-console.log(page);
+        console.log(1)
         
+
+
   
 </script>
 
