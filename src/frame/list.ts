668e7f9c1a37631eac46c92d6c049fef63c06b14
LBC-PC\lbc15 <1585638808@qq.com>
2023-07-15 10:25:49 +0800

    update

--- src/frame/list.ts
+++ src/frame/list.ts
@@ -1,17 +1,35 @@
 
 import gt from "lodash/gt";
+import gte from "lodash/gte";
 import lt from "lodash/lt";
+import lte from "lodash/lte";
 import eq from "lodash/eq";
+
+function not(v1, v2) {
+    return v1 != v2
+}
+
+function notAndType(v1, v2) {
+    return v1 !== v2
+}
+
+function or(v1, v2) {
+    return v1 || v2
+}
+
 
 let funMap = {
     gt,
+    gte,
     lt,
-    eq
+    lte,
+    eq,
+    or,
+    not
 }
 
 export function con(item, index, key, funName, ...args) {
     let fun = funMap[funName];
-    // console.log(funName, args);
     return fun(_.get(item, key), ...args);
 }
 
